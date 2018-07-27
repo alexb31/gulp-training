599789e40d10d84719851cacce00ad207e37e951
alexb31 <alexandrebornstein@gmail.com>
2018-07-27 11:16:10 +0200

    First Gulp Task

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+
+// Styles
+gulp.task('styles', function () {
+    console.log('starting styles task');
+});
+
+// Scripts
+gulp.task('scripts', function() {
+    console.log('starting scripts task');
+});
+
+// Images
+gulp.task('images', function() {
+    console.log('starting images task');
+});
+
+gulp.task('default', function() {
+    console.log('starting default');
+})
