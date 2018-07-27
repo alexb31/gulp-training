212edeb087b1568a3f63b3f6bbf2b13323f7b360
alexb31 <alexandrebornstein@gmail.com>
2018-07-27 16:02:21 +0200

    Minify Css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var livereload = require('gulp-livereload');
 var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
 
 // File Path
 var DIST_PATH = 'public/dist';
@@ -14,6 +15,7 @@ gulp.task('styles', function () {
     console.log('starting styles task');
     return gulp.src(['public/css/reset.css', CSS_PATH])
         .pipe(concat('styles.css'))
+        .pipe(minifyCss())
         .pipe(gulp.dest(DIST_PATH+'/css'))
         .pipe(livereload());
 });
@@ -41,4 +43,5 @@ gulp.task('watch', function () {
     require('./server.js');
     livereload.listen();
     gulp.watch(SCRIPTS_PATH, ['scripts'])
+    gulp.watch(CSS_PATH, ['styles'])
 });
